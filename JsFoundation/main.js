ad922d235421a4534540169947fb57be46dd7da3
Shantanu Gondane <113772055+shantanugondane@users.noreply.github.com>
2025-07-10 19:51:52 +0530

    2 things learned loops and let var const

---
+++ JsFoundation/main.js
@@ -0,0 +1,4 @@
+let FirstName = "shantanmu";
+let age = 21;
+let isStudent = true;
+console.log("this is the person whose name is " + FirstName + " and age is " + age + " and isStudent is " + isStudent);
